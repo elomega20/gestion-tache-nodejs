6272cf159e9563107f06c8039b049b87bb9a6d80
El Sedy Ba <elsedy000@gmail.com>
2024-08-06 15:12:04 +0200

    creation des controllers

---
+++ controllers/tache-controller.js
@@ -0,0 +1,25 @@
+const Thing = require('../models/tache');
+
+exports.creeTache = (req, res, next) => {
+    const thing = new Thing({
+        nomTache: req.body.nomTache,
+        description: req.body.description,
+        priorite: req.body.priorite,
+        echeance: req.body.echeance,
+        status: req.body.status,
+        responsable: req.body.responsable
+    });
+    thing.save().then(
+        () => {
+            res.status(201).json({
+                message: 'tache cree avec success!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
+};
